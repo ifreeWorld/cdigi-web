100a56a6a2b7b3576b801ca0c4bc577de09492c6
wangzilong <wangzilong@sensorsdata.cn>
2022-09-15 17:55:30 +0800

    提交

---
+++ packages/client/src/pages/analysis/create/data.d.ts
@@ -0,0 +1,67 @@
+/**
+ * 数据透视的数据结构
+ */
+export interface PivotData {
+  filter: PivotFilter[];
+  row: PivotRow;
+  line: PivotLine;
+  value: PivotValue;
+}
+
+export type PivotFilterValue = string | number | boolean;
+
+/**
+ * 筛选
+ */
+export type PivotFilter =
+  | {
+      field: string;
+      op: '=';
+      value: PivotFilterValue;
+    }
+  | {
+      field: string;
+      op: 'in';
+      value: PivotFilterValue[];
+    }
+  | {
+      field: string;
+      op: 'between';
+      value: [PivotFilterValue, PivotFilterValue];
+    };
+
+/**
+ * 行
+ */
+export interface PivotRow {
+  field: string;
+
+  /**
+   * 行的筛选
+   */
+  filter: PivotFilter;
+}
+
+/**
+ * 列
+ */
+export interface PivotLine {
+  field: string;
+
+  /**
+   * 列的筛选
+   */
+  filter: PivotFilter;
+}
+
+/**
+ * 值
+ */
+export interface PivotValue {
+  field: string;
+
+  /**
+   * 聚合类型
+   */
+  aggregator: 'sum' | 'avg' | 'max' | 'min';
+}
